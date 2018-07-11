ddb71c1c2385ab840cf2230956757a82b08019ce
qing-ren-zi <qing-ren.zi@hpe.com>
2018-07-11 18:50:37 +0800

    delete annotation

--- src/examples/cifar10_cnn/index.ts
+++ src/examples/cifar10_cnn/index.ts
@@ -3,9 +3,6 @@ import { Cifar10 } from '../../datasets/cifar10'
 import { ChartBatchLog, ChartEpochLog } from '../../utils/charts'
 
 const BATCH_SIZE = 16
-const TRAIN_BATCHES = 780
-const TEST_BATCH_SIZE = 1000
-const TEST_ITERATION_FREQUENCY = 5
 
 const model = tf.sequential()
 model.add(tf.layers.conv2d({
@@ -96,44 +93,7 @@ async function train (data: Cifar10) {
       }
     })
 
-  console.log(history.history)
-  // Call dispose on the training/test tensors to free their GPU memory.
-
-  // tf.nextFrame() returns a promise that resolves at the next call to
-  // requestAnimationFrame(). By awaiting this promise we keep our model
-  // training from blocking the main UI thread and freezing the browser.
   await tf.nextFrame()
-
-  // console.time('Totol training time')
-  // const history = await model.fit(trainX, trainY, {
-  //   validationData: [testX, testY],
-  //   batchSize: 128,
-  //   epochs: 5,
-  //   callbacks: {
-  //     onBatchEnd: (epoch: number, log) => {
-  //       console.log('batch:', epoch, log)
-  //       if (epoch % 10 === 0) chartBatch.updata(log)
-  //       return tf.nextFrame()
-  //     },
-  //     onEpochBegin: (epoch, log) => {
-  //       console.time('Epoch training time')
-  //       console.groupCollapsed('epoch times:', epoch)
-  //       return tf.nextFrame()
-  //     },
-  //     onEpochEnd: (epoch, log) => {
-  //       console.groupEnd()
-  //       console.timeEnd('Epoch training time')
-  //       console.log(epoch, log)
-  //       chartEpoch.updata(log)
-  //       return tf.nextFrame()
-  //     }
-  //   }
-  // })
-
-  // const score = model.evaluate(testX, testY)
-  // console.timeEnd('Totol training time')
-  // score[0].print()
-  // score[1].print()
 }
 
 async function load () {
